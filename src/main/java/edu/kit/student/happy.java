3055406c5c22eeba894f522d833b3e7941d48f39
uhjfh_glb <uhjfh_glb@student.kit.edu>
2020-05-28 11:54:34 +0200

    edit on branch hot_fix

--- src/main/java/edu/kit/student/happy.java
+++ src/main/java/edu/kit/student/happy.java
@@ -3,6 +3,6 @@ package edu.kit.student;
 public class happy {
  public static void main(String[] args) {
 	 System.out.println("right");
-	 System.out.println("left");
+	 System.out.println("hot fix......");
  }
 }
